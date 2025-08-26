70da350a614136e740973faeb6b27b7ea0a73732
Kender <kendermendoza591@gmail.com>
2025-08-26 13:47:37 +0200

    Added functionality

--- app/javascript/dashboard/routes/dashboard/settings/agents/Index.vue
+++ app/javascript/dashboard/routes/dashboard/settings/agents/Index.vue
@@ -291,6 +291,7 @@ const confirmDeletion = () => {
         :email="currentAgent.email"
         :availability="currentAgent.availability_status"
         :custom-role-id="currentAgent.custom_role_id"
+        :agent="currentAgent"
         @close="hideEditPopup"
       />
     </woot-modal>

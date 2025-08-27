3180e59b7849672630850be47543b40278896eb1
Kender <kendermendoza591@gmail.com>
2025-08-27 13:06:42 +0200

    - work timezone

--- app/javascript/dashboard/routes/dashboard/settings/agents/EditAgent.vue
+++ app/javascript/dashboard/routes/dashboard/settings/agents/EditAgent.vue
@@ -131,7 +131,7 @@ const editAgent = async () => {
       id: props.id,
       name: agentName.value,
       availability: agentAvailability.value,
-      working_hours: dtos,
+      ...dtos,
     };
 
     if (selectedRole.value.name.startsWith('custom_')) {

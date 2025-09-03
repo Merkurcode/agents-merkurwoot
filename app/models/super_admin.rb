2dee89fe3a86c43277c149a26bca6d0bc274390a
Kender <kendermendoza591@gmail.com>
2025-09-03 14:45:42 +0200

    - refactor - testcases - added functionality to setting profile

--- app/models/super_admin.rb
+++ app/models/super_admin.rb
@@ -23,7 +23,6 @@
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string
 #  sign_in_count          :integer          default(0), not null
-#  timezone               :string           default("UTC")
 #  tokens                 :json
 #  type                   :string
 #  ui_settings            :jsonb

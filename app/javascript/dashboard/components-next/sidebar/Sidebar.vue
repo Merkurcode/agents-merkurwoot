c5c3bd188d2988a59bb3e3c7d25d8d4e59fc7c0e
Kender <kendermendoza591@gmail.com>
2025-11-03 20:50:15 +0100

    - Change CRM List for Pipelines

--- app/javascript/dashboard/components-next/sidebar/Sidebar.vue
+++ app/javascript/dashboard/components-next/sidebar/Sidebar.vue
@@ -167,7 +167,7 @@ const menuItems = computed(() => {
           activeOn: ['conversations_through_folders'],
           children: conversationCustomViews.value.map(view => ({
             name: `${view.name}-${view.id}`,
-            label: view.is_board ? `CRM: ${view.name}` : view.name,
+            label: view.name,
             to: accountScopedRoute('folder_conversations', { id: view.id }),
           })),
         },

412c5bd6da0d1d953c753d4851c47b51246bf688
phan26 <kendermendoza591@gmail.com>
2025-11-28 16:22:27 +0100

    feat: add ActsAsTaggableOn tag creation  (#44)

--- spec/services/data_import/tags_manager_spec.rb
+++ spec/services/data_import/tags_manager_spec.rb
@@ -45,9 +45,10 @@ RSpec.describe DataImport::TagsManager do
 
         taggings = manager.build(identifier: '123', tags: 'ruby, invalid_tag')
 
-        expect(taggings.size).to eq(2)
+        expect(taggings.size).to eq(1)
         expect(taggings.all?(ActsAsTaggableOn::Tagging)).to be true
         expect(taggings.map(&:taggable_id).uniq).to eq([contact.id])
+        expect(taggings.first.tag_id).to eq(ActsAsTaggableOn::Tag.find_by(name: 'ruby').id)
       end
     end
 

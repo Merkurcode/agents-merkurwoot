a5e959c5980fdfc0b9eadcd204e4c7429440f010
phan26 <kendermendoza591@gmail.com>
2025-12-15 20:45:14 +0100

    refactor: remove redundant phone number validation logic in AddAgent and EditAgent components (#58)

--- app/javascript/dashboard/routes/dashboard/settings/agents/EditAgent.vue
+++ app/javascript/dashboard/routes/dashboard/settings/agents/EditAgent.vue
@@ -73,10 +73,6 @@ const selectedRoleId = ref(props.customRoleId || props.type);
 const agentCredentials = ref({ email: props.email });
 const selectedResponsibleId = ref(props.responsibleId);
 
-const parsedPhoneNumber = computed(() => {
-  return parsePhoneNumber(agentPhoneNumber.value || '');
-});
-
 const isPhoneNumberNotValid = computed(() => {
   if (agentPhoneNumber.value !== '') {
     return (
@@ -85,18 +81,6 @@ const isPhoneNumberNotValid = computed(() => {
     );
   }
   return false;
-});
-
-const setPhoneNumber = computed(() => {
-  if (parsedPhoneNumber.value && parsedPhoneNumber.value.countryCallingCode) {
-    return agentPhoneNumber.value;
-  }
-  if (agentPhoneNumber.value === '' && activeDialCode.value !== '') {
-    return '';
-  }
-  return activeDialCode.value
-    ? `${activeDialCode.value}${agentPhoneNumber.value}`
-    : '';
 });
 
 const setPhoneCode = code => {
@@ -119,6 +103,22 @@ if (props.phoneNumber) {
     agentPhoneNumber.value = props.phoneNumber;
   }
 }
+
+const parsedPhoneNumber = computed(() => {
+  return parsePhoneNumber(agentPhoneNumber.value || '');
+});
+
+const setPhoneNumber = computed(() => {
+  if (parsedPhoneNumber.value && parsedPhoneNumber.value.countryCallingCode) {
+    return agentPhoneNumber.value;
+  }
+  if (agentPhoneNumber.value === '' && activeDialCode.value !== '') {
+    return '';
+  }
+  return activeDialCode.value
+    ? `${activeDialCode.value}${agentPhoneNumber.value}`
+    : '';
+});
 
 const rules = {
   agentName: { required, minLength: minLength(1) },

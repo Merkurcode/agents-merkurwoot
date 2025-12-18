c8d78e4089692091882d712cce59cc28da7e364e
Joel Cano <joel.alarcon1@gmail.com>
2025-12-18 17:44:15 -0600

    fix: remove duplicate assignee_agent_bot_id column in schema

--- db/schema.rb
+++ db/schema.rb
@@ -790,7 +790,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_16_002044) do
     t.bigint "pipeline_status_id"
     t.bigint "assignee_agent_bot_id"
     t.integer "conversation_type", default: 0, null: false
-    t.bigint "assignee_agent_bot_id"
     t.index ["account_id", "display_id"], name: "index_conversations_on_account_id_and_display_id", unique: true
     t.index ["account_id", "id"], name: "index_conversations_on_id_and_account_id"
     t.index ["account_id", "inbox_id", "status", "assignee_id"], name: "conv_acid_inbid_stat_asgnid_idx"

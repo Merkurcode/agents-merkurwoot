30ccdf74202c249e43997e6ce383fc436f347286
phan26 <kendermendoza591@gmail.com>
2026-01-06 18:09:14 +0100

    feat: alias for chatwoot sdk (#65)
    - update SDK references from chatwoot to nautoConsole for consistency

--- app/models/channel/web_widget.rb
+++ app/models/channel/web_widget.rb
@@ -75,7 +75,7 @@ class Channel::WebWidget < ApplicationRecord
         g.async = true;
         s.parentNode.insertBefore(g,s);
         g.onload=function(){
-          window.chatwootSDK.run({
+          window.nautoConsoleSDK.run({
             websiteToken: '#{website_token}',
             baseUrl: BASE_URL
           })

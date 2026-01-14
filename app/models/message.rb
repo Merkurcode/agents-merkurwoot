abe997ad87030c598d83ccdf99c5e1841c284954
Joel Cano Alarcón <joel.Alarcon1@gmail.com>
2026-01-14 15:30:19 -0600

    feat: copilots v2 webhooks sms and ai responses (#70)
    * feat: Add last_chat_message_at to conversations
    
    * feat: Enhance Lead Retargeting logic and structure
    
    * feat: Enhancing copilot feature UI
    
    * fix: visualization data
    
    * feat: add inbox phone number on webhook

--- app/models/message.rb
+++ app/models/message.rb
@@ -441,7 +441,13 @@ class Message < ApplicationRecord
 
   def set_conversation_activity
     # rubocop:disable Rails/SkipsModelValidations
-    conversation.update_columns(last_activity_at: created_at, updated_at: Time.current)
+    update_attrs = { last_activity_at: created_at }
+
+    unless activity?
+      update_attrs[:last_chat_message_at] = created_at
+    end
+
+    conversation.update_columns(update_attrs)
     # rubocop:enable Rails/SkipsModelValidations
   end
 

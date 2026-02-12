0b055f3654f6672322df04662e10b3248b80e343
Joel Cano <joel.alarcon1@gmail.com>
2026-02-12 15:28:38 -0600

    feat: implement lead creation orchestration and listener

--- app/dispatchers/async_dispatcher.rb
+++ app/dispatchers/async_dispatcher.rb
@@ -16,6 +16,7 @@ class AsyncDispatcher < BaseDispatcher
       CsatSurveyListener.instance,
       HookListener.instance,
       InstallationWebhookListener.instance,
+      LeadCreationOrchestratorListener.instance,
       LeadFollowUpListener.instance,
       NotificationListener.instance,
       ParticipationListener.instance,

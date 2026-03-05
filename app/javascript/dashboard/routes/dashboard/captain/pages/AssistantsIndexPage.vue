d40c38c644c0ab7fa5834a3647add26dcf673775
Kender <kendermendoza591@gmail.com>
2026-03-05 18:23:33 +0100

    feat: Captain to external agent
    remove FAQs and Documents from Captain sidebar menu; update routing fallback

--- app/javascript/dashboard/routes/dashboard/captain/pages/AssistantsIndexPage.vue
+++ app/javascript/dashboard/routes/dashboard/captain/pages/AssistantsIndexPage.vue
@@ -53,8 +53,6 @@ const routeToLastActiveAssistant = () => {
 
   const { navigationPath } = route.params;
   const isAValidRoute = [
-    'captain_assistants_responses_index', // Faq page
-    'captain_assistants_documents_index', // Document page
     'captain_assistants_scenarios_index', // Scenario page
     'captain_assistants_playground_index', // Playground page
     'captain_assistants_inboxes_index', // Inboxes page
@@ -64,7 +62,7 @@ const routeToLastActiveAssistant = () => {
 
   const navigateTo = isAValidRoute
     ? navigationPath
-    : 'captain_assistants_responses_index';
+    : 'captain_assistants_playground_index';
 
   return routeToView(navigateTo, {
     accountId: route.params.accountId,

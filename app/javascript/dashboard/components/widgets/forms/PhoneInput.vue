196086cb6d4dcef96a8d0f0670edfdecd428aabe
Kender <kendermendoza591@gmail.com>
2026-03-23 19:49:55 +0100

    fix(phone-input): ensure phone number starts with '+' and update active dial code handling fix(agent): adjust button margin and ensure full phone number is passed for country detection fix(weekly-availability): make timezone input wrap full width and adjust multiselect margin

--- app/javascript/dashboard/components/widgets/forms/PhoneInput.vue
+++ app/javascript/dashboard/components/widgets/forms/PhoneInput.vue
@@ -148,11 +148,12 @@ export default {
     },
     setActiveCountry() {
       const { phoneNumber } = this;
-      if (!phoneNumber) return;
+      if (!phoneNumber || !phoneNumber.startsWith('+')) return;
       const number = parsePhoneNumber(phoneNumber);
       if (number) {
         this.activeCountryCode = number.country;
-        this.activeDialCode = number.countryCallingCode;
+        this.activeDialCode = `+${number.countryCallingCode}`;
+        this.phoneNumber = phoneNumber.replace(`+${number.countryCallingCode}`, '');
       }
     },
     toggleCountryDropdown() {
